04d4902b6a0a01b938019df57ca1fd6f517246b8
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2024-10-31 13:39:52 +0530

    feat: moved secrets to .env

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 
 // Get the Google client ID from the environment variable
-const clientId = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID;
+const clientId = import.meta.env.VITE_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
